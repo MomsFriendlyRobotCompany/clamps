e10a6642373f5238427786b8f5b5b3ff15a42b68
walchko <walchko@users.noreply.github.com>
2022-08-27 19:48:31 -0600

    fix :whale:

--- clamps/__init__.py
+++ clamps/__init__.py
@@ -5,8 +5,9 @@
 # see LICENSE for full details
 ##############################################
 from .js import PS4Joystick
+from .js import PS5Joystick
 from .js import Joystick
-from .js import Axis, PS4Buttons, JS, JSInfo
+from .js import Axis, PSButtons, JS, JSInfo
 
 from importlib.metadata import version # type: ignore
 
